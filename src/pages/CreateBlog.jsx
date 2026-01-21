7239b76d7428871ea22081967dcde2648364c3c4
nabihakhan6t4 <khannabiha923@gmail.com>
2026-01-21 14:50:41 +0300

    changes

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from "react";
 import { Card } from "../components/ui/card";
 import { Label } from "../components/ui/label";
 import { Input } from "../components/ui/input";
@@ -10,44 +10,58 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "../components/ui/select"
-
-    <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+} from "../components/ui/select";
+import { Button } from "../components/ui/button";
 
 const CreateBlog = () => {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+
   return (
-    <div className='p-4 md:pr-20 h-screen md:ml-[320px] pt-20 '>
-     <Card className='md:p-10 p-4 dark:bg-gray-800'>
-      <h1 className='text-2xl font-bold'>Let's create a blog</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda modi fugiat nihil molestiae placeat, doloremque hic animi recusandae ab! Vel nihil rerum explicabo unde, obcaecati sapiente cum et alias tempora.</p>
-      <div className='mt-10'>
-        <div>
-          <Label>
-            Title
-          </Label>
-<Input type='text' placeholder='Your blog name ' className='bg-white dark:bg-gray-700'/>
+    <div className="p-4 md:pr-20 h-screen md:ml-[320px] pt-20 ">
+      <Card className="md:p-10 p-4 dark:bg-gray-800">
+        <h1 className="text-2xl font-bold">Let's create a blog</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          modi fugiat nihil molestiae placeat, doloremque hic animi recusandae
+          ab! Vel nihil rerum explicabo unde, obcaecati sapiente cum et alias
+          tempora.
+        </p>
+        <div className="mt-5">
+          <div>
+            <Label>Title</Label>
+            <Input
+              type="text"
+              placeholder="Your blog name "
+              className="bg-white dark:bg-gray-700 mt-1"
+            />
+          </div>
+          <div className="mt-4 mb-5">
+            <Label className="mb-1">Category</Label>
+
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select category" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Blog Categories</SelectLabel>
+                  <SelectItem value="technology">Technology</SelectItem>
+                  <SelectItem value="programming">Programming</SelectItem>
+                  <SelectItem value="design">Design</SelectItem>
+                  <SelectItem value="lifestyle">Lifestyle</SelectItem>
+                  <SelectItem value="education">Education</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex gap-2">
+            <Button>Create</Button>
+          </div>
         </div>
-
-      </div>
-
-     </Card>
-      
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default CreateBlog
+export default CreateBlog;
